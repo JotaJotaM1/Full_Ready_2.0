3b3aae8b8b8910f467c0e9498d346507ad5cb5ed
JotaJotaM1 <johnjames_89@hotmail.com>
2024-11-19 21:08:12 -0500

    The Discover page is created, and all the media queries are adapted.

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,6 +1,12 @@
 import '../styles/pages/Discorver.css';
 import frasco3 from '../assets/frasco3.png'
 import FAQAccordion from '../components/FAQacordeon';
+import {
+    step1,
+    step2,
+    step3,
+    step4,
+} from '../assets/imgHome';
 
 
 const Discovers = () => {
@@ -13,6 +19,46 @@ const Discovers = () => {
                         <img className='imgBannderOne' src={frasco3}></img>
                     </div>
                     <FAQAccordion />
+                </div>
+            </section>
+            <section>
+                <div className="cnt-stp">
+                    <div>
+                        <h2 className="titlleStep">FOLLOW THESE SIMPLE STEPS FOR THE BEST RESULTS</h2>
+                        <p className="textStep">Amet pellentesque tellus velit risus. Semper habitant nulla ullamcorper</p>
+                    </div>
+                    <div className="cnt-FatherImgStep">
+                        <div className="cnt-imgStepp">
+                            <img src={step1}></img>
+                            <h3 className="titileImgStep">Step 1</h3>
+                            <p className="textImgStep">Nullam auctor dignissim pelntesque tristique quis.</p>
+                            <a className="learMoreStep">LEAR MORE<i className="bi bi-arrow-right-short"></i></a>
+                        </div>
+                        <div className="cnt-imgStepp">
+                            <img src={step2}></img>
+                            <h3 className="titileImgStep">Step 2</h3>
+                            <p className="textImgStep">Nullam auctor dignissim pelntesque tristique quis.</p>
+                            <a className="learMoreStep">LEAR MORE<i className="bi bi-arrow-right-short"></i></a>
+                        </div>
+                        <div className="cnt-imgStepp">
+                            <img src={step3}></img>
+                            <h3 className="titileImgStep">Step 3</h3>
+                            <p className="textImgStep">Nullam auctor dignissim pelntesque tristique quis.</p>
+                            <a className="learMoreStep">LEAR MORE<i className="bi bi-arrow-right-short"></i></a>
+                        </div>
+                        <div className="cnt-imgStepp">
+                            <img src={step4}></img>
+                            <h3 className="titileImgStep">Step 4</h3>
+                            <p className="textImgStep">Nullam auctor dignissim pelntesque tristique quis.</p>
+                            <a className="learMoreStep">LEAR MORE<i className="bi bi-arrow-right-short"></i></a>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className='titleTwoDisco'>DO YOU HAVE QUESTIONS?</h2>
+                    </div>
+                    <div>
+                        <button className="btn-Discover">CONTAC US<i className="arrowDiscover bi-arrow-bar-right"></i></button>
+                    </div>
                 </div>
             </section>
         </main>
