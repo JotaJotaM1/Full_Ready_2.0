acc7696ee842934afac5dc90703c52f96cb61175
JotaJotaM1 <johnjames_89@hotmail.com>
2024-11-21 17:57:27 -0500

    pruena de despliegue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? 'Full_Ready_2.0' : '/',
+  base: command === 'build' ? '/home' : '/',
 }));
