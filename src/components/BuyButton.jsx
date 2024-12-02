9a134f9edace9ab4b0890a52f7ba16cea3e4b910
JotaJotaM1 <johnjames_89@hotmail.com>
2024-12-02 14:07:31 -0500

    Store code is modified to be used with Full Ready's official email.

--- src/components/BuyButton.jsx
+++ src/components/BuyButton.jsx
@@ -1,4 +1,4 @@
-import{ useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import Client from 'shopify-buy';
 
 const ShopifyBuyButton = () => {
@@ -27,8 +27,8 @@ const ShopifyBuyButton = () => {
 
         const ShopifyBuyInit = () => {
             const client = Client.buildClient({
-                domain: 'sumv3i-pn.myshopify.com',
-                storefrontAccessToken: '25fb168a3fd455b3635baf7189cdaae6',
+                domain: 'dftbwf-16.myshopify.com', // Nuevo dominio
+                storefrontAccessToken: '00688922017476acf1a9cb6fc763d4ec', // Nuevo token
             });
 
             if (window.ShopifyBuy && window.ShopifyBuy.UI) {
@@ -37,7 +37,7 @@ const ShopifyBuyButton = () => {
                         productComponentRef.current.innerHTML = '';
                     }
                     ui.createComponent('product', {
-                        id: '8119855546564',
+                        id: '8831350243567', // Nuevo ID de producto
                         node: productComponentRef.current,
                         moneyFormat: '%24%7B%7Bamount_with_comma_separator%7D%7D',
                         options: {
@@ -47,263 +47,80 @@ const ShopifyBuyButton = () => {
                                         '@media (min-width: 601px)': {
                                             'max-width': '100%',
                                             'margin-left': '0',
-                                            'margin-bottom': '50px'
+                                            'margin-bottom': '50px',
                                         },
-                                        'text-align': 'left'
+                                        'text-align': 'left',
                                     },
                                     title: {
-                                        'font-family': 'Source Sans Pro, sans-serif',
-                                        'font-size': '31px',
-                                        'color': '#000000'
+                                        'font-size': '26px',
+                                        color: '#000000',
                                     },
                                     button: {
-                                        'font-family': 'Montserrat, sans-serif',
-                                        'font-weight': 'bold',
                                         ':hover': {
-                                            'background-color': '#000000'
+                                            'background-color': '#000000',
                                         },
                                         'background-color': '#000000',
                                         ':focus': {
-                                            'background-color': '#000000'
+                                            'background-color': '#000000',
                                         },
-                                        'border-radius': '5px',
-                                        'padding-left': '92px',
-                                        'padding-right': '92px'
+                                        'border-radius': '4px',
                                     },
                                     price: {
-                                        'font-family': 'Source Sans Pro, sans-serif',
-                                        'font-weight': 'bold',
-                                        'font-size': '16px'
+                                        'font-size': '18px',
+                                        color: '#000000',
                                     },
                                     compareAt: {
-                                        'font-family': 'Source Sans Pro, sans-serif',
-                                        'font-weight': 'bold',
-                                        'font-size': '13.6px'
+                                        'font-size': '15.3px',
+                                        color: '#000000',
                                     },
                                     unitPrice: {
-                                        'font-family': 'Source Sans Pro, sans-serif',
-                                        'font-weight': 'bold',
-                                        'font-size': '13.6px'
+                                        'font-size': '15.3px',
+                                        color: '#000000',
                                     },
-                                    description: {
-                                        'font-family': 'Roboto, sans-serif',
-                                        'font-weight': 'bold',
-                                        'font-size': '15px'
-                                    }
                                 },
                                 layout: 'horizontal',
                                 contents: {
                                     img: false,
                                     imgWithCarousel: true,
-                                    description: true
+                                    description: true,
                                 },
                                 width: '100%',
                                 text: {
-                                    button: 'Add to cart'
+                                    button: 'Add to cart',
                                 },
-                                googleFonts: [
-                                    'Source Sans Pro',
-                                    'Roboto',
-                                    'Montserrat'
-                                ]
-                            },
-                            productSet: {
-                                styles: {
-                                    products: {
-                                        '@media (min-width: 601px)': {
-                                            'margin-left': '-20px'
-                                        }
-                                    }
-                                }
-                            },
-                            modalProduct: {
-                                contents: {
-                                    img: false,
-                                    imgWithCarousel: true,
-                                    button: false,
-                                    buttonWithQuantity: true
-                                },
-                                styles: {
-                                    product: {
-                                        '@media (min-width: 601px)': {
-                                            'max-width': '100%',
-                                            'margin-left': '0px',
-                                            'margin-bottom': '0px'
-                                        }
-                                    },
-                                    button: {
-                                        'font-family': 'Montserrat, sans-serif',
-                                        'font-weight': 'bold',
-                                        ':hover': {
-                                            'background-color': '#000000'
-                                        },
-                                        'background-color': '#000000',
-                                        ':focus': {
-                                            'background-color': '#000000'
-                                        },
-                                        'border-radius': '5px',
-                                        'padding-left': '92px',
-                                        'padding-right': '92px'
-                                    },
-                                    title: {
-                                        'font-family': 'Helvetica Neue, sans-serif',
-                                        'font-weight': 'bold',
-                                        'font-size': '26px',
-                                        'color': '#4c4c4c'
-                                    },
-                                    price: {
-                                        'font-family': 'Helvetica Neue, sans-serif',
-                                        'font-weight': 'normal',
-                                        'font-size': '18px',
-                                        'color': '#4c4c4c'
-                                    },
-                                    compareAt: {
-                                        'font-family': 'Helvetica Neue, sans-serif',
-                                        'font-weight': 'normal',
-                                        'font-size': '15.3px',
-                                        'color': '#4c4c4c'
-                                    },
-                                    unitPrice: {
-                                        'font-family': 'Helvetica Neue, sans-serif',
-                                        'font-weight': 'normal',
-                                        'font-size': '15.3px',
-                                        'color': '#4c4c4c'
-                                    },
-                                    description: {
-                                        'font-family': 'Helvetica Neue, sans-serif',
-                                        'font-weight': 'normal',
-                                        'font-size': '14px',
-                                        'color': '#4c4c4c'
-                                    }
-                                },
-                                googleFonts: [
-                                    'Montserrat'
-                                ],
-                                text: {
-                                    button: 'Add to cart'
-                                }
                             },
                             cart: {
                                 styles: {
                                     button: {
-                                        'font-family': 'Montserrat, sans-serif',
-                                        'font-weight': 'bold',
                                         ':hover': {
-                                            'background-color': '#000000'
+                                            'background-color': '#000000',
                                         },
                                         'background-color': '#000000',
                                         ':focus': {
-                                            'background-color': '#000000'
+                                            'background-color': '#000000',
                                         },
-                                        'border-radius': '5px'
+                                        'border-radius': '4px',
                                     },
-                                    title: {
-                                        'color': '#000000'
-                                    },
-                                    header: {
-                                        'color': '#000000'
-                                    },
-                                    lineItems: {
-                                        'color': '#000000'
-                                    },
-                                    subtotalText: {
-                                        'color': '#000000'
-                                    },
-                                    subtotal: {
-                                        'color': '#000000'
-                                    },
-                                    notice: {
-                                        'color': '#000000'
-                                    },
-                                    currency: {
-                                        'color': '#000000'
-                                    },
-                                    close: {
-                                        'color': '#000000',
-                                        ':hover': {
-                                            'color': '#000000'
-                                        }
-                                    },
-                                    empty: {
-                                        'color': '#000000'
-                                    },
-                                    noteDescription: {
-                                        'color': '#000000'
-                                    },
-                                    discountText: {
-                                        'color': '#000000'
-                                    },
-                                    discountIcon: {
-                                        'fill': '#000000'
-                                    },
-                                    discountAmount: {
-                                        'color': '#000000'
-                                    }
                                 },
                                 text: {
                                     total: 'Subtotal',
-                                    button: 'Checkout'
+                                    button: 'Checkout',
                                 },
-                                googleFonts: [
-                                    'Montserrat'
-                                ]
                             },
                             toggle: {
                                 styles: {
                                     toggle: {
-                                        'font-family': 'Montserrat, sans-serif',
-                                        'font-weight': 'bold',
                                         'background-color': '#000000',
                                         ':hover': {
-                                            'background-color': '#000000'
+                                            'background-color': '#000000',
                                         },
                                         ':focus': {
-                                            'background-color': '#000000'
-                                        }
-                                    }
+                                            'background-color': '#000000',
+                                        },
+                                    },
                                 },
-                                googleFonts: [
-                                    'Montserrat'
-                                ]
                             },
-                            lineItem: {
-                                styles: {
-                                    variantTitle: {
-                                        'color': '#000000'
-                                    },
-                                    title: {
-                                        'color': '#000000'
-                                    },
-                                    price: {
-                                        'color': '#000000'
-                                    },
-                                    fullPrice: {
-                                        'color': '#000000'
-                                    },
-                                    discount: {
-                                        'color': '#000000'
-                                    },
-                                    discountIcon: {
-                                        'fill': '#000000'
-                                    },
-                                    quantity: {
-                                        'color': '#000000'
-                                    },
-                                    quantityIncrement: {
-                                        'color': '#000000',
-                                        'border-color': '#000000'
-                                    },
-                                    quantityDecrement: {
-                                        'color': '#000000',
-                                        'border-color': '#000000'
-                                    },
-                                    quantityInput: {
-                                        'color': '#000000',
-                                        'border-color': '#000000'
-                                    }
-                                }
-                            }
-                        }
+                        },
                     });
                 });
             }
@@ -320,7 +137,21 @@ const ShopifyBuyButton = () => {
         }
     }, []);
 
-    return <div className="shopify-buy-button-container" ref={productComponentRef} id="product-component-1732771509579" style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px', background: '#fff', borderRadius: '10px' }}></div>;
+    return (
+        <div
+            className="shopify-buy-button-container"
+            ref={productComponentRef}
+            id="product-component-1733165895053"
+            style={{
+                maxWidth: '1200px',
+                margin: '0 auto',
+                padding: '20px',
+                background: '#fff',
+                borderRadius: '10px',
+            }}
+        ></div>
+    );
 };
 
 export default ShopifyBuyButton;
+
