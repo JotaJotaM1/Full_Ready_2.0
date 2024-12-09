aaa7e5a9cd85c854f7d5fef968f95b45be5e657b
JotaJotaM1 <johnjames_89@hotmail.com>
2024-12-09 17:46:31 -0500

    Adjust navbar styling for a unique home page look based on Figma design.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,45 +15,49 @@ const NavBar = () => {
 
     return (
         <header>
-            <div className={navbarClass}>
-                <div className="nav_logo">
-                    <Link to="/home">
-                        <img src={logo} alt="Logo" />
-                    </Link>
-                </div>
+            <div>
+                <div className={navbarClass}>
+                    <div className="nav_logo">
+                        <Link to="/home">
+                            <img src={logo} alt="Logo" />
+                        </Link>
+                    </div>
 
-                <div className={`nav_items ${isOpen ? "open" : ""}`}>
-                    <div className='cnt-Border'>
-                    <ul>
-                        <li className={location.pathname === '/home' ? 'active' : ''}>
-                            <Link to="/home" onClick={handleLinkClick}>HOME</Link>
-                        </li>
-                        <li className={location.pathname === '/discover' ? 'active' : ''}>
-                            <Link to="/discover" onClick={handleLinkClick}>DISCOVER</Link>
-                        </li>
-                        <li className={location.pathname === '/shop' ? 'active' : ''}>
-                            <Link to="/shop" onClick={handleLinkClick}>SHOP</Link>
-                        </li>
-                        {/* <li className={location.pathname === '/find-full-ready' ? 'active' : ''}>
+                    <div className={`nav_items ${isOpen ? "open" : ""}`}>
+                        <div className='cnt-Border'>
+                            <ul>
+                                <li className={location.pathname === '/home' ? 'active' : ''}>
+                                    <Link to="/home" onClick={handleLinkClick}>HOME</Link>
+                                </li>
+                                <li className={location.pathname === '/discover' ? 'active' : ''}>
+                                    <Link to="/discover" onClick={handleLinkClick}>DISCOVER</Link>
+                                </li>
+                                <li className={location.pathname === '/shop' ? 'active' : ''}>
+                                    <Link to="/shop" onClick={handleLinkClick}>SHOP</Link>
+                                </li>
+                                {/* <li className={location.pathname === '/find-full-ready' ? 'active' : ''}>
                             <Link to="/find-full-ready" onClick={handleLinkClick}>FIND FULL READY</Link>
                         </li>
                         <li className={location.pathname === '/explore-more' ? 'active' : ''}>
                             <Link to="/explore-more" onClick={handleLinkClick}>EXPLORE MORE</Link>
                         </li> */}
-                        <li className={location.pathname === '/ambassadors' ? 'active' : ''}>
-                            <Link to="/ambassadors" onClick={handleLinkClick}>AMBASSADORS</Link>
-                        </li>
-                    </ul>
+                                <li className={location.pathname === '/ambassadors' ? 'active' : ''}>
+                                    <Link to="/ambassadors" onClick={handleLinkClick}>AMBASSADORS</Link>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
-                    
+
+                    <div className={`nav_toggle ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
 
-                <div className={`nav_toggle ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
             </div>
+
         </header>
     );
 };
